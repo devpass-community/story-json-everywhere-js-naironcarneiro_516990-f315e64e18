5e83d73adef54508a134c7fd6bc9a0c60648c77e
Nairon Carneiro <naironcarneiro@Nairons-Air.home>
2023-08-05 19:34:44 +0100

    feat: fix added call api

--- productService.js
+++ productService.js
@@ -1,11 +1,13 @@
+const axios = require("axios");
 async function getProduct(productId) {
   const apiUrl = `https://fakestoreapi.com/products/${productId}`;
 
-  axios.get(apiUrl, {
-    params: {
-      productId: productId,
-    },
-  });
+  try {
+    const response = await axios.get(apiUrl);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 }
 
 module.exports = getProduct;
